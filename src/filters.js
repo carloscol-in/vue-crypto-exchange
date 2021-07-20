8a2b4b011f1c86e3d6f9a2139e57bb834e896c4e
Carlos <contact@carloscolin.me>
2021-07-20 01:19:09 -0500

    created dollar and percent filters

--- src/filters.js
+++ src/filters.js
@@ -1,8 +1,20 @@
 import numeral from "numeral";
 
 
-const dollarFilter = () => {
+const dollarFilter = (value) => {
     if(!value) {
         return '$ 0';
     }
+
+    return numeral(value).format('($ 0.00a)');
 }
+
+const percentFilter = (value) => {
+    if(!value) {
+        return '0 %';
+    }
+
+    return `${Number(value).toFixed(2)}%`;
+}
+
+export { dollarFilter, percentFilter };
