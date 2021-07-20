e7cbf62fd83d2e15c3e310f3745d7348576e66c3
Carlos <contact@carloscolin.me>
2021-07-20 01:21:03 -0500

    added filters to app

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 
 import router from "@/router";
+import { dollarFilter, percentFilter } from "@/filters";
 
+// declare filters
+Vue.filter('dollar', dollarFilter);
+Vue.filter('percent', percentFilter);
 Vue.config.productionTip = false;
 
 new Vue({
