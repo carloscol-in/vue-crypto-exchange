6298e001ecf64be7cc4ea849b59b4c02487cb0b6
Carlos <contact@carloscolin.me>
2021-07-19 20:20:03 -0500

    created views dir

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex-col items-center">
+        <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+        <p class="text-gray-600 text-xl text-center">
+            Project to obtain the quotes from the most important crypto currencies.
+            <br>
+            This project was created as part of a Vue.js course on Platzi.
+        </p>
+
+        <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+            Go back to Home
+        </router-link>
+    </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex-col items-center">
+        <h1 class="text-gray-700 text-6xl uppercase">
+            error
+        </h1>
+        <router-link
+            to="/"
+            class="mt-5 text-xl text-green-600 hover:underline"
+        >
+            Go back to Home
+        </router-link>
+    </div>
+</template>
