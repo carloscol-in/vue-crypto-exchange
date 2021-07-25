993906496796fc643773cdcdc5f39736af73e5fa
Carlos <contact@carloscolin.me>
2021-07-25 12:32:30 -0500

    added getMarkets and getExchange methods

--- src/api.js
+++ src/api.js
@@ -6,6 +6,44 @@ function getAssets() {
         .then(res => res.data)
 }
 
+function getAsset(asset) {
+    let result = fetch(`${url}/assets/${asset}`)
+        .then(res => res.json())
+        .then(res => res.data)
+    return result;
+}
+
+function getAssetHistory(asset) {
+    const now = new Date();
+    const end = now.getTime();
+
+    now.setDate(now.getDate() - 1);
+
+    const start = now.getTime();
+
+    let result = fetch(`${url}/assets/${asset}/history?interval=h1&start=${start}&end=${end}`)
+        .then(res => res.json())
+        .then(res => res.data);
+
+    return result;
+}
+
+function getMarkets(coin) {
+    return fetch(`${url}/assets/${coin}/markets?limit=5`)
+        .then(res => res.json())
+        .then(res => res.data)
+}
+
+function getExchange(id) {
+    return fetch(`${url}/exchanges//${id}`)
+        .then(res => res.json())
+        .then(res => res.data)
+}
+
 export default {
-    getAssets
+    getAssets,
+    getAsset,
+    getAssetHistory,
+    getMarkets,
+    getExchange,
 }
